92e6ba1bc3240a68f8355161d6d527dd9fb65931
Harshit Pathak <hapatha@Harshits-MacBook-Pro-3.local>
2020-02-03 18:27:48 +0530

    completing with next level

--- pairs/main.js
+++ pairs/main.js
@@ -2,7 +2,7 @@ const colors = [
     'red',
     'green',
     'blue',
-    'cyan',
+    'magenta',
     'black',
     'brown',
     'orange',
@@ -35,6 +35,7 @@ const gameEngine = {
     currentLevel: 1,
     maxLevel: 3,
     gameGrid: undefined,
+    gameGridBag: undefined,
     reset: () => {
         gameEngine.lastClickedTile = undefined;
         gameEngine.lastClickedTileHTML = undefined;
@@ -42,10 +43,27 @@ const gameEngine = {
         gameEngine.currentLevelMoves = 0;
         gameEngine.currentLevel = 1;
         gameEngine.maxLevel = 3;
+        gameEngine.gameGrid = undefined;
+        gameEngine.gameGridBag = undefined;
+        gameEngine.cleanGridContainer();
+        gameEngine.cleanLevelNotifier();
+    },
+    cleanGridContainer: () => {
+        let gameGridContainer = document.getElementById('gameGrid');
+        gameGridContainer.innerHTML = '';
+    },
+    cleanLevelNotifier: () => {
+        let levelNotifierHTMLElem = document.getElementById('levelNotifier');
+        levelNotifierHTMLElem.innerHTML = '';
     },
     init: () => {
         gameEngine.reset();
         gameEngine.startLevel(1);
+        let nextLevelButton = document.getElementById('nextLevelButton');
+        nextLevelButton.addEventListener('click', () => {
+            gameEngine.nextLevel();
+            gameEngine.hideNextLevelButton();
+        });
     },
     startLevel: level => {
         let startingLevel = level || gameEngine.currentLevel;
@@ -53,6 +71,7 @@ const gameEngine = {
     },
     renderLevel: level => {
         gameEngine.gameGrid = gameEngine.prepareGridForLevel(level);
+        gameEngine.gameGridBag = gameEngine.prepareGridBagForLevel(level);
         gameEngine.renderGrid(gameEngine.gameGrid);
         gameEngine.renderLevelNotifier(level);
     },
@@ -88,6 +107,19 @@ const gameEngine = {
 
         return gameGrid;
     },
+    prepareGridBagForLevel: level => {
+        let gridBag = {};
+        let numberOfGrids = level * 2;
+        for (let i = 0; i < numberOfGrids; i++) {
+            for (let j = 0; j < numberOfGrids; j++) {
+                gridBag[`${i}-${j}`] = true;
+            }
+        }
+        return gridBag;
+    },
+    removeFromGridBag: (i, j) => {
+        delete gameEngine.gameGridBag[`${i}-${j}`];
+    },
     prepareTile: (i, j, revealColor) => {
         return {
             originalColor: 'yellow',
@@ -101,6 +133,7 @@ const gameEngine = {
         let gameGridContainer = document.getElementById('gameGrid');
         for (let i = 0; i < gameGrid.length; i++) {
             let tileRowHTML = document.createElement('div');
+            tileRowHTML.classList.add('flex');
             for (let j = 0; j < gameGrid[i].length; j++) {
                 let tile = gameGrid[i][j];
                 let tileHTML = gameEngine.prepareTileHTML(tile);
@@ -153,6 +186,11 @@ const gameEngine = {
             if (tile.revealColor === gameEngine.lastClickedTile.revealColor) {
                 tile.memorised = true;
                 gameEngine.lastClickedTile.memorised = true;
+                gameEngine.removeFromGridBag(tile.row, tile.column);
+                gameEngine.removeFromGridBag(
+                    gameEngine.lastClickedTile.row,
+                    gameEngine.lastClickedTile.column
+                );
             } else {
                 tile.memorised = false;
                 gameEngine.lastClickedTile.memorised = false;
@@ -160,7 +198,7 @@ const gameEngine = {
                 setTimeout(() => {
                     lastTileClickedHTMl.style.backgroundColor = 'yellow';
                     tileHTMLElem.style.backgroundColor = 'yellow';
-                }, 500);
+                }, 400);
             }
             gameEngine.lastClickedTile = undefined;
             gameEngine.lastClickedTileHTML = undefined;
@@ -171,8 +209,40 @@ const gameEngine = {
             gameEngine.lastClickedTileHTML = tileHTMLElem;
             gameEngine.matchingMode = true;
         }
+        if (gameEngine.checkIfLevelFinished()) {
+            // Enable Next Level Button
+            gameEngine.showNextLevelButton();
+        }
     },
-    nextLevel: () => {},
+    showNextLevelButton: () => {
+        let nextLevelButton = document.getElementById('nextLevelButton');
+        nextLevelButton.classList.remove('hidden');
+    },
+    hideNextLevelButton: () => {
+        let nextLevelButton = document.getElementById('nextLevelButton');
+        nextLevelButton.classList.add('hidden');
+    },
+    nextLevel: () => {
+        if (gameEngine.currentLevel === gameEngine.maxLevel) {
+            alert('Congratulations You Commpleted All levels!');
+            return;
+        } else {
+            gameEngine.cleanGridContainer();
+            gameEngine.cleanLevelNotifier();
+            gameEngine.currentLevel++;
+            gameEngine.startLevel(gameEngine.currentLevel);
+        }
+    },
+    checkIfLevelFinished: () => {
+        if (
+            gameEngine.gameGridBag &&
+            Object.keys(gameEngine.gameGridBag).length === 0
+        ) {
+            // every tile is removed i.e the level is solved
+            return true;
+        }
+        return false;
+    },
 };
 
 (function init() {
